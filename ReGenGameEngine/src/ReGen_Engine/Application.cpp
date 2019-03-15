ab57b4f69b72bf134176919e43d5f4716dc4ff1d
Rink <rinkwilbrink1@gmail.com>
2019-03-15 10:46:10 +0100

    Window Creation.
    The window is being created now, You can change the color in Application.cpp.
    Line 47 at this time of the development

--- ReGenGameEngine/src/ReGen_Engine/Application.cpp
+++ ReGenGameEngine/src/ReGen_Engine/Application.cpp
@@ -1,31 +1,64 @@
 #include "rgpch.h"
 #include "Application.h"
 #include "ReGen_Engine/Log.h"
-#include "Events/ApplicationEvent.h"
+//#include "Input.h"
+
+#include <glad/glad.h>
 
 namespace ReGenGames
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
+	Application* Application::s_Instance = nullptr;
+
 	Application::Application()
 	{
+		RG_CORE_ASSERT(!s_Instance, "Application already exists!");
+		s_Instance = this;
+
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		/* Layer stacking
+		for (auto it = m_LayerStack.end(); it < m_LayerStack.begin();)
+		{
+			(*--it)->OnEvent(e);
+			if(e.Handled)
+				break;
+		}
+		*/
+	}
+
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
-		{
-			CORE_TRACE(e);
-		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			CORE_INFO(e);
-		}
-
 		//While loop to keep the program running
-		while (true);
+		while (isRunning)
+		{
+			glClearColor(1, 1, 1, 0); // Background Colour
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			/*
+			for(Layer* layer : m_LayerStack)
+				layer->OnUpdate();
+			*/
+
+			m_Window->OnUpdate();
+		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		isRunning = false;
+		return true;
 	}
 }
--- ReGenGameEngine/src/ReGen_Engine/Application.h
+++ ReGenGameEngine/src/ReGen_Engine/Application.h
@@ -1,7 +1,10 @@
 #pragma once
-
 #include "Core.h"
+
+#include "Window.h"
+//#include "ReGenGameEngine/LayerStack.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace ReGenGames
 {
@@ -12,6 +15,19 @@ namespace ReGenGames
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+
+		inline Window& GetWindow() { return *m_Window;  }
+
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
+		std::unique_ptr<Window> m_Window;
+		bool isRunning = true;
+		//LayerStack m_LayerStack;
+	private:
+		static Application* s_Instance;
 	};
 
 	//Needs to bedefinded in a client application code.
